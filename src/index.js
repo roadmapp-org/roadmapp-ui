7688e2f564a7aff115c766fb6632b378b63346ba
Tobias Olea <tobiolea97@gmail.com>
2023-11-18 11:32:20 -0300

    Setup Redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import App from './App'
+import store from './data/store'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root')
+const root = createRoot(container)
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
+  </Provider>,
+)
