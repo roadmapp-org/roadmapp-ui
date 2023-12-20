342583ab49023a13e12a118c3817a3bc71a4c011
Tobias Olea <tobiolea97@gmail.com>
2023-12-19 23:50:37 -0300

    Filter tasks by project on Config page

--- src/components/config/task-list-component.js
+++ src/components/config/task-list-component.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { ProjectListItemComponent } from './project-list-item'
 import { TaskListItemComponent } from './task-list-item-component'
@@ -6,11 +6,24 @@ import { TaskListItemComponent } from './task-list-item-component'
 export const TaskListComponent = () => {
     const projects = useSelector((state) => state.levels.projects)
     const tasks = useSelector((state) => state.levels.tasks)
+    const [filteredTask, setFilteredTasks] = useState([]);
+
+    const onSelectProject = (e) => {
+        if(e.target.value !== "0") {
+            setFilteredTasks(tasks.filter((item) => item.project_id.toString() === e.target.value))
+        } else {
+            setFilteredTasks(tasks)
+        }
+    }
+
+    useEffect(() => {
+        setFilteredTasks(tasks)
+    }, [tasks])
 
     return (
         <>
-        <select>
-            <option value="0" key={0}>Select a project</option>
+        <select onChange={onSelectProject}>
+            <option value="0" key={0}>All</option>
             {projects.map((project) => (
                 <option value={project.id} key={project.id}>{project.name}</option>
             ))}
@@ -26,7 +39,7 @@ export const TaskListComponent = () => {
                 </thead>
                 <tbody>
                     {
-                        tasks.map((task,index) => (
+                        filteredTask.map((task,index) => (
                             <TaskListItemComponent task={task} key={task.id}/>
                         ))
                     }
