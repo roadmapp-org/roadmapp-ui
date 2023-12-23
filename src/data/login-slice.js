43d5961f0c8c4e55295999f59465a0935405c1da
Tobias Olea <tobiolea97@gmail.com>
2023-12-23 15:50:21 -0300

    Login error management improvements.

--- src/data/login-slice.js
+++ src/data/login-slice.js
@@ -4,23 +4,22 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 export const login = createAsyncThunk('auth/login', async (credentials) => {
         let response;
         try {
-            
             response = await fetch('http://localhost:8080/auth/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(credentials)
             });
-
         } catch (error) {
-            throw new Error('Server error.');
+            throw new Error('Server error. Try again later.');
         }
+
+        if(response.status === 401) throw new Error('Invalid user or password.');
 
         const token = response.headers.get('authorization');
 
-        if(token !== null && token !== undefined && token !== '')
-            return token;
-        
-        throw new Error('Wrong credentials.');
+        if(token !== null && token !== undefined && token !== '') return token;
+
+        throw new Error('Unexpected error. Try again later.');
     }
 );
 
