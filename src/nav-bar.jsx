9de2e702a70fc448d39d5939a2533f202e70a147
Tobias Olea <tobiolea97@gmail.com>
2024-04-22 08:29:25 -0300

    Nav bar. Burger menu links working.

--- src/nav-bar.jsx
+++ src/nav-bar.jsx
@@ -40,18 +40,22 @@ export const NavBar = () => {
                         </div>
                     </nav>
                 </header>
-                <div className={`w-100 h-100 bg-custom-black text-custom-white ${isOpen ? 'block' : 'hidden'} block xs:hidden`} id="menu">
-                    <a className="text-xl flex justify-end p-4 pr-10 border-t border-rose-500">
-                        <span>Home</span>
-                        <img src="home.png" className="w-6 h-6 rounded self-center ml-4" />
-                    </a>
-                    <a className="text-xl flex justify-end p-4 pr-10 border-t border-gray-300">
-                        Config
-                        <img src="config.png" className="w-6 h-6 rounded self-center ml-4" />
-                    </a>
-                    <a className="text-xl flex justify-end p-4 pr-10 border-t border-gray-300">
+                <div className={`w-100 h-100 bg-custom-black text-custom-white ${isOpen ? 'block' : 'hidden'}`} id="menu">
+                    <Link to="/home" onClick={() => setIsOpen(!isOpen)}>
+                        <a className="text-xl flex justify-end p-4 pr-10 border-t border-gray-300">
+                            <span>Home</span>
+                            <img src="home.png" className="w-6 h-6 rounded self-center ml-4" />
+                        </a>
+                    </Link>
+                    <Link to="/config" onClick={() => setIsOpen(!isOpen)}>
+                        <a className="text-xl flex justify-end p-4 pr-10 border-t border-gray-300">
+                            <span>Config</span>
+                            <img src="home.png" className="w-6 h-6 rounded self-center ml-4" />
+                        </a>
+                    </Link>
+                    <a className="text-xl flex justify-end p-4 pr-10 border-t border-gray-300" onClick={onClickLogOut}>
                         <span>Logout</span>
-                        <img src="logout.png" className="w-6 h-6 rounded self-center ml-4" />
+                        <img src="home.png" className="w-6 h-6 rounded self-center ml-4" />
                     </a>
                 </div>
             </>
