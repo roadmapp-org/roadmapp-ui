5d5436a735fcbfa92266efe790e8a43a8ffd0558
Tobias Olea <tobiolea97@gmail.com>
2024-04-23 08:14:29 -0300

    Log filter component. Chips based filtering. Layout working!

--- src/nav-bar.jsx
+++ src/nav-bar.jsx
@@ -50,12 +50,12 @@ export const NavBar = () => {
                     <Link to="/config" onClick={() => setIsOpen(!isOpen)}>
                         <a className="text-xl flex justify-end p-4 pr-10 border-t border-gray-300">
                             <span>Config</span>
-                            <img src="home.png" className="w-6 h-6 rounded self-center ml-4" />
+                            <img src="config.png" className="w-6 h-6 rounded self-center ml-4" />
                         </a>
                     </Link>
                     <a className="text-xl flex justify-end p-4 pr-10 border-t border-gray-300" onClick={onClickLogOut}>
                         <span>Logout</span>
-                        <img src="home.png" className="w-6 h-6 rounded self-center ml-4" />
+                        <img src="logout.png" className="w-6 h-6 rounded self-center ml-4" />
                     </a>
                 </div>
             </>
