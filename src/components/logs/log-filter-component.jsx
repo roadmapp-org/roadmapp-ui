7a32b206f18db511b546425336bd21d74d6f34da
Tobias Olea <tobiolea97@gmail.com>
2024-04-24 18:26:11 -0300

    Clean projects/task menues when the filter is erased.

--- src/components/logs/log-filter-component.jsx
+++ src/components/logs/log-filter-component.jsx
@@ -36,10 +36,19 @@ export const LogFilterComponent = () => {
       }
     }, [])
 
+    useEffect(() => {
+        if(currentProject === 0)
+            document.getElementById("projectsList").value = 0;
+        if(currentTask === 0) {
+            document.getElementById("taskList").value = 0;
+        }
+
+      }, [currentProject, currentTask])
+
     return (
         <>
             <div className={`mb-5 flex flex-col items-center justify-center ${currentProject == 0 ? "block" : "hidden"}`}>
-                <h1 className='text-2xl mb-4'>Projects</h1>
+                <h1 className='text-2xl mb-4 self-start'>Projects</h1>
                 <div className="flex items-start">
                     <select className="w-full flex flex-wrap justify-evenly rounded-md py-2 bg-custom-white shadow" id="projectsList" onChange={onClickProject}>
                         <option key={0} value={0} className={`relative align-content-center flex-grow text-center text-custom-black p-2 m-1 rounded-md font-medium text-black hover:cursor-pointer`}>{"All projects"}</option>
@@ -57,10 +66,10 @@ export const LogFilterComponent = () => {
                 </div>
             </div>
             <div className={`mb-5 flex flex-col items-center justify-center ${currentProject != 0 && currentTask == 0 ? "block" : "hidden"}`}>
-                <div className='text-2xl flex mb-5'>
+                <div className='text-2xl flex mb-5 self-start'>
                     <span className='self-center text-center'>Tasks</span>
                 </div>
-                <select className="w-full rounded-md py-2 bg-custom-white shadow" id="projectsList" onChange={onClickTask}>
+                <select className="w-full rounded-md py-2 bg-custom-white shadow" id="taskList" onChange={onClickTask}>
                     <option key={0} value={0} className={`relative align-content-center flex-grow text-center text-custom-black p-2 m-1 rounded-md font-medium text-black hover:cursor-pointer`}>{"All tasks"}</option>
                     {
                         filteredTask.map((item, index) => (
